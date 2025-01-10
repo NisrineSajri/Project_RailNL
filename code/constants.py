668da5182c4ec78c624a12a225bc736c2e6550b2
NisrineSajri <nisrine.sajri@student.uva.nl>
2025-01-10 11:06:05 +0100

    niks aangepast

--- code/constants.py
+++ code/constants.py
@@ -7,3 +7,5 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 STATIONS_FILE = os.path.join(DATA_DIR, "StationsHolland.csv")
 CONNECTIONS_FILE = os.path.join(DATA_DIR, "ConnectiesHolland.csv")
+
+""
